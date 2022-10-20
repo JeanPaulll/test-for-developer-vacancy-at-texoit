36d634dca8caa8ea8761c4905e1e56dc13268c6b
Jean Paul <jeanpaulwebb@gmail.com>
2022-10-20 02:01:04 -0300

    :sparkles: Component for navbar with active menu

---
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -0,0 +1,22 @@
+import {Component, OnInit} from '@angular/core';
+
+/**
+ * @author Jean Paul - <jeanpaulwebb@gmail.com>
+ * @class NavbarComponent
+ * @description Component for navbar with active menu
+ * @date 20/10/2022
+ */
+@Component({
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
+
+}
