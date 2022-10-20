7bca9153b72823acf2a00991bbad31028dc0b55f
Jean Paul <jeanpaulwebb@gmail.com>
2022-10-20 01:57:11 -0300

    :sparkles: Creating component for routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {DashboardComponent} from "./views/pages/dashboard/dashboard.component";
+import {MoviesComponent} from "./views/pages/movies/movies.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'dasboard', component: DashboardComponent},
+  {path: 'movies', component: MoviesComponent},
+  {path: "**", redirectTo: "dasboard", pathMatch: "full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
