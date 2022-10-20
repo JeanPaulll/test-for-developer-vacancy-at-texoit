9ea88245b82e21b3ec38a28fd6d504870407b1bb
Jean Paul <jeanpaulwebb@gmail.com>
2022-10-20 02:03:16 -0300

    :sparkles: Component to show the movie listing screen

---
+++ src/app/views/pages/movies/movies.component.ts
@@ -0,0 +1,22 @@
+import {Component, OnInit} from '@angular/core';
+
+/**
+ * @author Jean Paul - <jeanpaulwebb@gmail.com>
+ * @class MoviesComponent
+ * @description Component to show the movie listing screen
+ * @date 20/10/2022
+ */
+@Component({
+    selector: 'app-movies',
+    templateUrl: './movies.component.html',
+    styleUrls: ['./movies.component.scss']
+})
+export class MoviesComponent implements OnInit {
+
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
+
+}
