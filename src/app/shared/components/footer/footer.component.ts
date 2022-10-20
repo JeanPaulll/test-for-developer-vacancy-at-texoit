d0e572867b8e1b939604bff810c626e54f4590fa
Jean Paul <jeanpaulwebb@gmail.com>
2022-10-20 02:00:34 -0300

    :sparkles: Component to bottom of page

---
+++ src/app/shared/components/footer/footer.component.ts
@@ -0,0 +1,22 @@
+import {Component, OnInit} from '@angular/core';
+
+/**
+ * @author Jean Paul - <jeanpaulwebb@gmail.com>
+ * @class FooterComponent
+ * @description Component to bottom of page
+ * @date 20/10/2022
+ */
+@Component({
+    selector: 'app-footer',
+    templateUrl: './footer.component.html',
+    styleUrls: ['./footer.component.scss']
+})
+export class FooterComponent implements OnInit {
+
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
+
+}
