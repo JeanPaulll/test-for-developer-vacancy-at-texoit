a8b56542cfdb6653a3d3002a47be0b2133e035cc
Jean Paul <jeanpaulwebb@gmail.com>
2022-10-20 02:02:31 -0300

    :sparkles: Component to show the dashboard screen

---
+++ src/app/views/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,22 @@
+import {Component, OnInit} from '@angular/core';
+
+/**
+ * @author Jean Paul - <jeanpaulwebb@gmail.com>
+ * @class DashboardComponent
+ * @description Component to show the dashboard screen
+ * @date 20/10/2022
+ */
+@Component({
+    selector: 'app-dashboard',
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
+
+}
